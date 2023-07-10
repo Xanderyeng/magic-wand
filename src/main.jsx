a543337c520ee181fbe7fbf113b1ab182c8bef5d
Alexander Chepkiyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-07-10 08:35:59 +0300

    Update main to load wand v1

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import App from './AppV1.jsx'
 import './index.css'
-//import Wand from './WandV2.jsx'
+import Wand from './Wand.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-   {* <Wand /> *}
+   {* <App /> *}
+    <Wand />
     
   </React.StrictMode>,
 )
